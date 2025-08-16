c67f9a0553ee9c9af48dfc46767f0edda0f71416
TheWover <17090738+TheWover@users.noreply.github.com>
2025-08-16 11:46:19 -0600

    attempt to resolve linter and pytest issues

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -2,7 +2,7 @@
 Database base configuration
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from app.core.config import settings
 
